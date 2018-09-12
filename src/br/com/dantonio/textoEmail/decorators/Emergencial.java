49d8e9dd7040815cdd04b82d9cbeca9eb5c8c574
diego.ferreira <diego.ferreira@consensotec.com.br>
2018-09-12 14:17:30 -0300

    Adição do Label emergencial

---
+++ src/br/com/dantonio/textoEmail/decorators/Emergencial.java
@@ -0,0 +1,11 @@
+package br.com.dantonio.textoEmail.decorators;
+
+import br.com.dantonio.textoEmail.baseClasses.Email;
+
+public class Emergencial extends EmailDecorator {
+
+	public Emergencial(Email email) {
+		super(email);
+	}
+
+}
