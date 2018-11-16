6b570474b9652860cf711bfbef50292ed6f5ca62
diego.ferreira <diego.ferreira@consensotec.com.br>
2018-11-16 17:41:30 -0300

    mudancas no ControladorEmail

--- src/br/com/dantonio/emailController/EmailController.java
+++ src/br/com/dantonio/emailController/EmailController.java
@@ -2,14 +2,35 @@ package br.com.dantonio.emailController;
 
 import br.com.dantonio.helpers.HelperEnvioEmail;
 
+/** 
+ * Classe que funciona como um controlador validando regra de
+ * negocio e criando os objetos corretamente para o envio das 
+ * vers&otilde;es.
+ 
+ * @author Diego.Ferreira
+ *
+ */
 public class EmailController {
 	
 	
-	public static String enviarEmailVersao(HelperEnvioEmail helper) {
-		StringBuilder sb = new StringBuilder("");
+	/**
+	 *  M&eacute;todo central respons&aacute;vel por fazer a
+	 *  cria&ccedil;&atilde;o, valida&ccedil;&atilde;o 
+	 *  e envio da versao para os clientes.
+	 *  
+	 * @param helper - {@link HelperEnvioEmail} criado com os 
+	 * parametro informados nas 3 abas.
+	 */
+	public static void enviarEmailVersao(HelperEnvioEmail helper) {
+		//Detectar os tipos de versao que serao disponibilizados
 		
+		//Validar a versao de acordo com o tipo informado
 		
-		return sb.toString();
+		//Checar se eh teste ou nao.
+		
+		//Caso esteja ok, validar se foi informado destinatarios, release notes e credenciais
+		// do email de envio
+
 	}
 
 }
